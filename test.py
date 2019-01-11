7cec576d71925e97e85d8dcb4f81498885b1549b
tmwatchanan <tmwatchanan@gmail.com>
2019-01-11 14:24:03 +0700

    Add unet evaluation with all weights being zeros

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from data import *
 from keras.utils import plot_model
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
 import os
+from PIL import Image
 
 image_filename = 'E-1-1.jpg'
-DATASET_NAME = 'eye_graylayer'
+DATASET_NAME = 'unet_check'
 WEIGHT_NAME = "unet-eye-10"
 COLOR = 'rgb' # rgb, grayscale
 INPUT_SIZE = (256, 256)
@@ -21,9 +21,20 @@ graylayer_dir = os.path.join(dataset_dir, "graylayer")
 weight_dir = os.path.join(dataset_dir, 'weights')
 image_dir = os.path.join(dataset_dir, 'dataset')
 
+images_dir = os.path.join(dataset_dir, 'images')
+labels_dir = os.path.join(dataset_dir, 'labels')
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
 weights_file = os.path.join(weight_dir, f"{WEIGHT_NAME}.hdf5")
 model_figure_file = os.path.join(dataset_dir, f"{WEIGHT_NAME}-model.png")
 image_test_file = os.path.join(image_dir, image_filename)
+
+## unet_check
+
+check_test1 = os.path.join(images_dir, 'E-1-1.jpg')
+check_label1 = os.path.join(labels_dir,'M-1-1.jpg')
 
 ## Run ------------------------------------------------------------------------
 
@@ -76,15 +87,6 @@ def test(model):
     saveResult(save_dir, results, file_names=test_files, weights_name=WEIGHT_NAME)
 
 
-def create_model():
-    if COLOR == 'rgb':
-        input_size = INPUT_SIZE + (3,)
-    elif COLOR == 'gray':
-        input_size = INPUT_SIZE + (1,)
-    model = unet(pretrained_weights=weights_file, input_size=input_size) # load pretrained model
-    return model
-
-
 def read_image(file_path):
     imread_flag = None
     if COLOR == 'rgb':
@@ -103,8 +105,74 @@ def read_image(file_path):
     return img
 
 
+def import_image(filename, color):
+    #  img = Image.open(filename).convert("LA")
+    #  img_np = np.array(img)[:,:,0]
+    #  return img_np
+    if color == 'rgb':
+        #  imread_flag = cv2.IMREAD_color
+        imread_flag = 1
+    elif color == 'grayscale':
+        #  imread_flag = cv2.IMREAD_GRAYSCALE
+        imread_flag = 0
+    img = cv2.imread(filename, imread_flag)
+    img = img / 255
+    img = trans.resize(img, TARGET_SIZE)
+    #  img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
+    img = np.reshape(img,img.shape+(1,)) if color == 'grayscale' else img
+    #  img = np.reshape(img,(1,)+img.shape)
+    return img
+
+
+def read_images(images_dir, color):
+    file_list = [f for f in listdir(images_dir) if isfile(join(images_dir, f))]
+    images = []
+    for file_name in file_list:
+        file_path = os.path.join(images_dir, file_name)
+        img = import_image(file_path, color)
+        images.append(img)
+    return images
+
+def bw_eval():
+    model = create_model(model_name='unet_check')
+    #  x = import_image(check_test1)
+    #  y = import_image(check_label1)
+    #  x_predicted = model.predict(x)
+    #  print(x_predicted)
+
+    images = read_images(images_dir, color='rgb')
+    labels = read_images(labels_dir, color='grayscale')
+    images = np.array(images)
+    labels = np.array(labels)
+    print("Shape of train images is: ", images.shape)
+    print("Shape of labels is: ", labels.shape)
+
+    #  print(images)
+    #  print(labels)
+
+    #  evaluation = model.evaluate(x=x, y=y, batch_size=2, verbose=1)
+    evaluation = model.evaluate(x=images, y=labels, batch_size=2, verbose=1)
+    print(evaluation)
+
+    #  test_files = [name for name in os.listdir(test_dir) if os.path.isfile(os.path.join(test_dir, name))]
+    #  num_test_files = len(test_files)
+    #  results = model.predict_generator(testGene, steps=num_test_files, verbose=1)
+    #  saveResult(save_dir, results, file_names=test_files, weights_name='zeros')
+
+
+def create_model(model_name):
+    if COLOR == 'rgb':
+        input_size = INPUT_SIZE + (3,)
+    elif COLOR == 'gray':
+        input_size = INPUT_SIZE + (1,)
+    if model_name == 'unet':
+        model = unet(pretrained_weights=weights_file, input_size=input_size) # load pretrained model
+    elif model_name =='unet_check':
+        model = unet_check(input_size=input_size)
+    return model
+
 def main():
-    model = create_model()
+    model = create_model(model_name='unet')
     #  test()
     #  img = read_image(image_test_file)
     #  layer_to_visualize(model, layer_name='conv2d_1', image=img) 
@@ -116,6 +184,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    #  main()
+    bw_eval()
 
 
