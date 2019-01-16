964ace034e25853a839969d21ee410264a1247e3
tmwatchanan <tmwatchanan@gmail.com>
2019-01-16 16:02:59 +0700

    Adapt the model for multi-class segmentation

--- data.py
+++ data.py
@@ -9,36 +9,30 @@ import skimage.io as io
 import skimage.transform as trans
 import cv2
 
-Sclera = [0, 128, 0]
-Sky = [128,128,128]
-Building = [128,0,0]
-Pole = [192,192,128]
-Road = [128,64,128]
-Pavement = [60,40,222]
-Tree = [128,128,0]
-SignSymbol = [192,128,128]
-Fence = [64,64,128]
-Car = [64,0,128]
-Pedestrian = [64,64,0]
-Bicyclist = [0,128,192]
+Background = [255,255,255]
+Sclera = [255,0,0]
+Iris = [0,255,0]
 Unlabelled = [0,0,0]
 
-COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
-                          Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
+COLOR_DICT = np.array([Background, Sclera, Iris, Unlabelled])
 
 
 def adjustData(img,mask,flag_multi_class,num_class):
     if(flag_multi_class):
+        #  print(mask.shape)
         img = img / 255
-        mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
-        new_mask = np.zeros(mask.shape + (num_class,))
-        for i in range(num_class):
+        #  mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
+        #  new_mask = np.zeros(mask.shape + (num_class,))
+        new_mask = mask
+        #  new_mask = np.zeros(mask.shape + (1,))
+        #  for i in range(num_class):
             #for one pixel in the image, find the class in mask and convert it into one-hot vector
             #index = np.where(mask == i)
             #index_mask = (index[0],index[1],index[2],np.zeros(len(index[0]),dtype = np.int64) + i) if (len(mask.shape) == 4) else (index[0],index[1],np.zeros(len(index[0]),dtype = np.int64) + i)
             #new_mask[index_mask] = 1
-            new_mask[mask == i,i] = 1
-        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
+            #  new_mask[mask == i,i] = 1
+            #  new_mask[mask == i,0] = i
+        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1],new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
         mask = new_mask
     elif(np.max(img) > 1):
         img = img / 255
@@ -46,7 +40,6 @@ def adjustData(img,mask,flag_multi_class,num_class):
         mask[mask > 0.5] = 1
         mask[mask <= 0.5] = 0
     return (img,mask)
-
 
 
 def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
@@ -121,16 +114,35 @@ def geneTrainNpy(image_path,mask_path,flag_multi_class = False,num_class = 2,ima
 
 
 def labelVisualize(num_class,color_dict,img):
-    img = img[:,:,0] if len(img.shape) == 3 else img
-    img_out = np.zeros(img.shape + (3,))
-    for i in range(num_class):
-        img_out[img == i,:] = color_dict[i]
-    return img_out / 255
+    #  img = img[:,:,0] if len(img.shape) == 3 else img
+    img_dim = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img_dim.shape + (3,))
+    print(img.shape)
+    print(img_out.shape)
+    mask = ((img[:,:,0] == 1) & (img[:,:,1] == 0) & (img[:,:,2] == 0))
+    img_out[mask, :] = color_dict[0]
+    mask = ((img[:,:,0] == 0) & (img[:,:,1] == 1) & (img[:,:,2] == 0))
+    img_out[mask, :] = color_dict[1]
+    mask = ((img[:,:,0] == 0) & (img[:,:,1] == 0) & (img[:,:,2] == 1))
+    img_out[mask, :] = color_dict[2]
+    #  for i in range(num_class):
+        #  img_out[img == i,:] = color_dict[i]
 
+        #  mask = ((img[:,:,0] == 1) & (img[:,:,1] == 0) & (img[:,:,2] == 0))
+        #  img_out[mask, :] = color_dict[i]
+
+        #  img_out[img == i, 0] = color_dict[i][0]
+        #  img_out[img == i, 1] = color_dict[i][1]
+        #  img_out[img == i, 2] = color_dict[i][2]
+    print(np.unique(img_out))
+    return img_out / 255
 
 
 def saveResult(save_path, npyfile, file_names, weights_name, flag_multi_class = False, num_class = 2):
     for i,item in enumerate(npyfile):
         print(i, file_names[i])
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
+        #  cv2.imshow('img', img)
+        #  cv2.waitKey(0)
         io.imsave(os.path.join(save_path, f"{file_names[i]}-{weights_name}.png"),img)
+
--- model.py
+++ model.py
@@ -54,7 +54,62 @@ def unet(pretrained_weights = None,input_size = (256,256,3), learning_rate = 1e-
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+    conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(input = inputs, output = conv10)
+
+    model.compile(optimizer = Adam(lr = learning_rate), loss = 'mean_squared_error', metrics = ['accuracy'])
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+
+def unet_check(pretrained_weights = None,input_size = (256,256,3), learning_rate = 1e-4):
+    inputs = Input(input_size)
+    
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'zeros', bias_initializer = 'zeros')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'relu')(conv9)
 
     model = Model(input = inputs, output = conv10)
 
@@ -66,5 +121,4 @@ def unet(pretrained_weights = None,input_size = (256,256,3), learning_rate = 1e-
     	model.load_weights(pretrained_weights)
 
     return model
-
 
