c460007d1ec5467c2cddade51cff1c5d787a927f
tmwatchanan <tmwatchanan@gmail.com>
2019-03-01 10:25:52 +0700

    Make experiment more detailed and organized

--- train.py
+++ train.py
@@ -1,24 +1,27 @@
 import os
+import datetime
 #  from model import unet_v2, ModelCheckpoint
-from model_baseline import baseline_v6_multiclass, ModelCheckpoint
-from data import trainGenerator, testGenerator, saveResult
+from model_baseline import baseline_v7_multiclass, ModelCheckpoint
+from data import trainGenerator, baseline_v7_test_generator, baseline_v7_save_result
 from keras.models import Model
+import numpy as np
 
-DATASET_NAME = 'eye-multiclass-baseline_v6-softmax-cce-lr1e_2'
-dataset_path = os.path.join('data', DATASET_NAME)
-COLOR = 'rgb'  # rgb, grayscale
+TRAIN_FLAG = True
+DATASET_NAME = 'eye_v2'
+MODEL_NAME = 'baseline_v7_multiclass'
+MODEL_INFO = 'softmax-cce-lw_421'
+LEARNING_RATE = "1e_3"
+EXPERIMENT_NAME = f"{DATASET_NAME}-{MODEL_NAME}-{MODEL_INFO}-lr_{LEARNING_RATE}"
+TEST_DIR_NAME = 'test'
 CONTINUED_WEIGHT = None  # "14", None
-weights_name = DATASET_NAME + "-{}"
-loss_acc_filename = f"{DATASET_NAME}-loss-acc.csv"
-loss_acc_file = os.path.join(dataset_path, loss_acc_filename)
 EPOCH_START = 1
-EPOCH_END = 4501
+EPOCH_END = 9001
 BATCH_SIZE = 6  # 10
 STEPS_PER_EPOCH = 1  # None
-LEARNING_RATE = 1e-3
 INPUT_SIZE = (256, 256)
 TARGET_SIZE = (256, 256)
 NUM_CLASSES = 3
+COLOR = 'rgb'  # rgb, grayscale
 
 if BATCH_SIZE > 10:
     answer = input(
@@ -27,6 +30,7 @@ if BATCH_SIZE > 10:
         print("You can change the value of BATCH_SIZE in this file")
         exit(1)
 
+dataset_path = os.path.join('data', EXPERIMENT_NAME)
 weights_dir = os.path.join(dataset_path, 'weights')
 training_set_dir = os.path.join(dataset_path, 'train')
 training_images_set_dir = os.path.join(training_set_dir, 'images')
@@ -36,9 +40,13 @@ validation_set_dir = os.path.join(dataset_path, 'validation')
 validation_images_set_dir = os.path.join(validation_set_dir, 'images')
 validation_labels_set_dir = os.path.join(validation_set_dir, 'labels')
 validation_aug_set_dir = os.path.join(validation_set_dir, 'augmentation')
-test_set_dir = os.path.join(dataset_path, 'test')
-predicted_set_dir = os.path.join(dataset_path, f"test-predicted-{COLOR}")
+test_set_dir = os.path.join(dataset_path, TEST_DIR_NAME)
+predicted_set_dir = os.path.join(dataset_path,
+                                 f"{TEST_DIR_NAME}-predicted-{COLOR}")
 mask_set_dir = os.path.join(dataset_path, f"mask-{COLOR}")
+loss_acc_file = os.path.join(dataset_path, 'loss-acc.csv')
+experiments_setting_file = os.path.join(dataset_path,
+                                        'experiment_settings.txt')
 
 if not os.path.exists(weights_dir):
     os.makedirs(weights_dir)
@@ -47,9 +55,32 @@ if not os.path.exists(predicted_set_dir):
 if not os.path.exists(mask_set_dir):
     os.makedirs(mask_set_dir)
 
+
+def save_experiment_settings_file():
+    with open(experiments_setting_file, "a") as f:
+        current_datetime = datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S")
+        f.write(f"{current_datetime}\n")
+        f.write(f"MODEL_NAME={MODEL_NAME}\n")
+        f.write(f"MODEL_INFO={MODEL_INFO}\n")
+        f.write(f"CONTINUE_WEIGHT={CONTINUED_WEIGHT}\n")
+        f.write(f"EPOCH_START={EPOCH_START}\n")
+        f.write(f"EPOCH_END={EPOCH_END}\n")
+        f.write(f"BATCH_SIZE={BATCH_SIZE}\n")
+        f.write(f"STEPS_PER_EPOCH={STEPS_PER_EPOCH}\n")
+        f.write(f"LEARNING_RATE={LEARNING_RATE}\n")
+        f.write(f"INPUT_SIZE={INPUT_SIZE}\n")
+        f.write(f"TARGET_SIZE={TARGET_SIZE}\n")
+        f.write(f"NUM_CLASSES={NUM_CLASSES}\n")
+        f.write(f"COLOR={COLOR}\n")
+        f.write(f"=======================\n")
+
+
+if TRAIN_FLAG:
+    save_experiment_settings_file()
+
 model_filename = "{}.hdf5"
 if CONTINUED_WEIGHT:
-    #  trained_weights_name = weights_name.format(CONTINUED_WEIGHT)
     trained_weights_name = CONTINUED_WEIGHT
     trained_weights_file = model_filename.format(trained_weights_name)
     trained_weights_file = os.path.join(weights_dir, trained_weights_file)
@@ -70,11 +101,12 @@ if COLOR == 'rgb':
     input_size = INPUT_SIZE + (5, )
 elif COLOR == 'grayscale':
     input_size = INPUT_SIZE + (1, )
-model, mask_model = baseline_v6_multiclass(
+learning_rate = float(LEARNING_RATE.replace("_", "-"))
+model, mask_model = globals()[MODEL_NAME](
     pretrained_weights=trained_weights_file,
     num_classes=NUM_CLASSES,
     input_size=input_size,
-    learning_rate=LEARNING_RATE)  # load pretrained model
+    learning_rate=learning_rate)  # load pretrained model
 
 #  model_file += "-{epoch:02d}-{val_acc:.2f}.hdf5"
 
@@ -121,78 +153,77 @@ loss_acc_list = []
 if not os.path.exists(loss_acc_file):
     with open(loss_acc_file, "w") as f:
         f.write(
-            'epoch,acc,val_acc,loss,val_loss,cce,val_cce,mae,val_mae,mape,val_mape\n'
+            'epoch,output1_acc,val_output1_acc,output2_acc,val_output2_acc,output3_acc,val_output3_acc\n'
         )
 
 # for each epoch
 for i in range(EPOCH_START, EPOCH_END):
     # train the model
-    #  new_weights_name = weights_name.format(str(i))
     new_weights_name = str(i)
-    new_weights_file = model_filename.format(new_weights_name)
-    new_weights_file = os.path.join(weights_dir, new_weights_file)
-    callbacks = None
-    if (i == 1) or (i % 100 == 0):
-        model_checkpoint = ModelCheckpoint(
-            filepath=new_weights_file,
-            monitor='val_acc',
-            mode='auto',
-            verbose=1,
-            save_best_only=False,
-            save_weights_only=False,
-            period=1)
-        callbacks = [model_checkpoint]
-    history = model.fit_generator(
-        train_gen,
-        steps_per_epoch=STEPS_PER_EPOCH,
-        epochs=1,
-        callbacks=callbacks,
-        validation_data=validation_gen,
-        validation_steps=num_validation,
-        workers=0,
-        use_multiprocessing=True)
-    print(history.history.keys())
-    trained_acc = history.history['acc'][-1]
-    trained_val_acc = history.history['val_acc'][-1]
-    trained_loss = history.history['loss'][-1]
-    trained_val_loss = history.history['val_loss'][-1]
-    trained_cce = history.history['categorical_crossentropy'][-1]
-    trained_val_cce = history.history['val_categorical_crossentropy'][-1]
-    trained_mae = history.history['mean_absolute_error'][-1]
-    trained_val_mae = history.history['val_mean_absolute_error'][-1]
-    trained_mape = history.history['mean_absolute_percentage_error'][-1]
-    trained_val_mape = history.history['val_mean_absolute_percentage_error'][
-        -1]
-    loss_acc = ','.join(
-        str(e) for e in [
-            i, trained_acc, trained_val_acc, trained_loss, trained_val_loss,
-            trained_cce, trained_val_cce, trained_mae, trained_val_mae,
-            trained_mape, trained_val_mape
-        ])
-    with open(loss_acc_file, "a") as f:
-        f.write(f"{loss_acc}\n")
+    if TRAIN_FLAG:
+        new_weights_file = model_filename.format(new_weights_name)
+        new_weights_file = os.path.join(weights_dir, new_weights_file)
+        callbacks = None
+        if (i == 1) or (i % 100 == 0):
+            model_checkpoint = ModelCheckpoint(
+                filepath=new_weights_file,
+                monitor='val_acc',
+                mode='auto',
+                verbose=1,
+                save_best_only=False,
+                save_weights_only=False,
+                period=1)
+            callbacks = [model_checkpoint]
+        history = model.fit_generator(
+            train_gen,
+            steps_per_epoch=STEPS_PER_EPOCH,
+            epochs=1,
+            callbacks=callbacks,
+            validation_data=validation_gen,
+            validation_steps=num_validation,
+            workers=0,
+            use_multiprocessing=True)
+        #  print(history.history.keys()) // show dict of metrics in history
+        trained_output1_acc = history.history['output1_acc'][-1]
+        trained_val_output1_acc = history.history['val_output1_acc'][-1]
+        trained_output2_acc = history.history['output2_acc'][-1]
+        trained_val_output2_acc = history.history['val_output2_acc'][-1]
+        trained_output3_acc = history.history['output3_acc'][-1]
+        trained_val_output3_acc = history.history['val_output3_acc'][-1]
+        loss_acc = ','.join(
+            str(e) for e in [
+                i,
+                trained_output1_acc,
+                trained_val_output1_acc,
+                trained_output2_acc,
+                trained_val_output2_acc,
+                trained_output3_acc,
+                trained_val_output3_acc,
+            ])
+        with open(loss_acc_file, "a") as f:
+            f.write(f"{loss_acc}\n")
+
     # test the model
-    test_gen_softmax = testGenerator(
+    test_gen_softmax = baseline_v7_test_generator(
         test_set_dir, target_size=TARGET_SIZE, color=COLOR)
     softmax_results = model.predict_generator(
         test_gen_softmax, steps=num_test_files, verbose=1)
     #  for idx, item in enumerate(softmax_results):
     #  print(item[16384, :])
 
-    test_gen = testGenerator(
+    test_gen = baseline_v7_test_generator(
         test_set_dir, target_size=TARGET_SIZE, color=COLOR)
     results = mask_model.predict_generator(
         test_gen, steps=num_test_files, verbose=1)
     print(test_files)
-    print(new_weights_name)
+    print(f"EPOCH# {new_weights_name}")
     if (i == 1) or (i % 100 == 0):
-        saveResult(
+        baseline_v7_save_result(
             predicted_set_dir,
             results,
             file_names=test_files,
             weights_name=new_weights_name,
             flag_multi_class=True,
             num_class=NUM_CLASSES)
-
-#  imgs_train,imgs_mask_train = geneTrainNpy("data/" + DATASET_NAME + "/train/aug/","data/" + DATASET_NAME + "/train/aug/")
-#  model.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+    if not TRAIN_FLAG:
+        break
