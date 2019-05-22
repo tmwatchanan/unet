Fix appending features to wrong order

utils.py
@@ -7,11 +7,11 @@ def add_position_layers(img, axis):
     size = (img.shape[1], img.shape[2])
     x_range = np.arange(size[0]) / (size[0] - 1)
     y_range = x_range.reshape(-1, 1) / (size[0] - 1)
-    X = Y = np.zeros(shape=[size[0], size[1]])
-    X[:, :] = x_range
-    Y[:, :] = y_range
-    img = np.insert(img, -1, X, axis=axis)
-    img = np.insert(img, -1, Y, axis=axis)
+    X = Y = np.zeros(shape=[size[0], size[1], 1])
+    X[:, :, 0] = x_range
+    Y[:, :, 0] = y_range
+    img = np.concatenate((img, X), axis=-1)
+    img = np.concatenate((img, Y), axis=-1)
     return img
 
 
@@ -31,17 +31,39 @@ def max_rgb_filter(image):
     return cv2.merge([B, G, R])
 
 
-def add_sobel_filters(img, axis):
-    img_gray = color.rgb2gray(img)
+def color_to_gray(img, color_model):
+    if color_model == "hsv":
+        # extract only the V layer
+        img_gray = img[:, :, 2]
+    else:
+        img_gray = color.rgb2gray(img)
+    return img_gray
+
+
+def add_sobel_filters(img, color_model, added_img):
+    img_gray = color_to_gray(img, color_model)
+    # extract sobel gradient feature layers
     im_sobel_h = filters.sobel_h(img_gray)
     im_sobel_v = filters.sobel_v(img_gray)
-    img = np.insert(img, -1, im_sobel_h, axis=axis)
-    img = np.insert(img, -1, im_sobel_v, axis=axis)
-    return img
+    # expand dimensions from (X, Y) to (X, Y, 1)
+    im_sobel_h = np.expand_dims(im_sobel_h, axis=-1)
+    im_sobel_v = np.expand_dims(im_sobel_v, axis=-1)
+    # concatenate feature layers into image
+    # (X, Y, 3) then becomes (X, Y, 5)
+    added_img = np.concatenate((added_img, im_sobel_h), axis=-1)
+    added_img = np.concatenate((added_img, im_sobel_v), axis=-1)
+    return added_img
 
 
-def add_canny_filter(img, axis, sigma):
-    img_gray = color.rgb2gray(img)
+def add_canny_filter(img, color_model, added_img, sigma):
+    img_gray = color_to_gray(img, color_model)
+    # extract canny edge feature layer = array of booleans
     im_canny = feature.canny(img_gray, sigma=sigma)
-    img = np.insert(img, -1, im_canny, axis=axis)
-    return img
+    # convert from boolean values to int values (0 or 1)
+    im_canny = im_canny.astype(int)
+    # expand dimensions from (X, Y) to (X, Y, 1)
+    im_canny = np.expand_dims(im_canny, axis=-1)
+    # concatenate a canny feature layer into image
+    # (X, Y, l) then becomes (X, Y, l+1)
+    added_img = np.concatenate((added_img, im_canny), axis=-1)
+    return added_img