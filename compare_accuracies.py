2de95d0eaa12345c933c07a27a8aac1458cb1358
tmwatchanan <tmwatchanan@gmail.com>
2019-06-10 14:34:02 +0700

    Add last 1000 values statistics

--- compare_accuracies.py
+++ compare_accuracies.py
@@ -10,23 +10,38 @@ def write_compared_accuracies():
     experiment_names = [
         "eye_v3-model_v12_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn",
         "eye_v3-model_v15_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn",
-        "eye_v3-s4-model_v26_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn",
     ]
+    v26_experiment_name = (
+        "eye_v3-s4-model_v26_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn"
+    )
     output_rows = []
     for fold in range(1, 16 + 1):
+        # read data from csv file
+        v26_experiment_path = os.path.join("data", v26_experiment_name.format(fold))
+        v26_training_log_file = os.path.join(v26_experiment_path, "training.csv")
+        v26_history_data = pd.read_csv(v26_training_log_file)
+        v26_val_output1_acc_list = v26_history_data["val_output1_acc"]
+        last_1000_values = v26_val_output1_acc_list[4000:5000].values
+        v26_last_1000_accuracy = np.mean(last_1000_values)
+        v26_last_1000_sd = np.std(last_1000_values)
+        v26_last_1000_min = np.min(last_1000_values)
+        v26_last_1000_max = np.max(last_1000_values)
         for epoch in range(499, 5000, 500):
             output_row = []
             output_row.append(fold)
             output_row.append(epoch + 1)
             for experiment_name in experiment_names:
-                # read data from csv file
                 dataset_path = os.path.join("data", experiment_name.format(fold))
                 training_log_file = os.path.join(dataset_path, "training.csv")
                 history_data = pd.read_csv(training_log_file)
-                # get accuracy of validation set at specific epoch
                 val_output1_acc_list = history_data["val_output1_acc"]
+                # get accuracy of validation set at specific epoch
                 fold_epoch_accuracy = val_output1_acc_list[epoch]
                 output_row.append(convert_to_percentage(fold_epoch_accuracy))
+            output_row.append(convert_to_percentage(v26_last_1000_accuracy))
+            output_row.append(format_percent(v26_last_1000_sd))
+            output_row.append(convert_to_percentage(v26_last_1000_min))
+            output_row.append(convert_to_percentage(v26_last_1000_max))
             output_rows.append(output_row)
 
     # # write outputs to file
@@ -38,7 +53,16 @@ def write_compared_accuracies():
             csv_f, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL
         )
         # prepare data
-        header_list = ["fold", "epoch", "model_v12", "model_v15", "model_v26"]
+        header_list = [
+            "fold",
+            "epoch",
+            "model_v12",
+            "model_v15",
+            "model_v26",
+            "sd",
+            "min",
+            "max",
+        ]
         csv_writer.writerow(header_list)
 
         for output_row in output_rows:
