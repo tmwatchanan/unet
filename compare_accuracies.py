98f37d61aaae818f7731eb58b03ef09272cf4b2a
tmwatchanan <tmwatchanan@gmail.com>
2019-06-11 10:45:16 +0700

    Add step size

--- compare_accuracies.py
+++ compare_accuracies.py
@@ -12,8 +12,9 @@ def write_compared_accuracies():
         "eye_v3-model_v15_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn",
     ]
     v26_experiment_name = (
-        "eye_v3-s4-model_v26_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn"
+        "eye_v3-s5-model_v27_multiclass-softmax-cce-lw_1_0.01-hsv-loo_{0}-lr_1e_2-bn"
     )
+    acc_csv_filename = f"model_v12_v15_v27.csv"
     output_rows = []
     for fold in range(1, 16 + 1):
         # read data from csv file
@@ -26,7 +27,8 @@ def write_compared_accuracies():
         v26_last_1000_sd = np.std(last_1000_values)
         v26_last_1000_min = np.min(last_1000_values)
         v26_last_1000_max = np.max(last_1000_values)
-        for epoch in range(499, 5000, 500):
+        STEP_SIZE = 100
+        for epoch in range(499, 5000, STEP_SIZE):
             output_row = []
             output_row.append(fold)
             output_row.append(epoch + 1)
@@ -45,8 +47,7 @@ def write_compared_accuracies():
             output_rows.append(output_row)
 
     # # write outputs to file
-    csv_dir = os.path.join("data", "comparison", "csv")
-    acc_csv_filename = f"model_v12_v15_v26.csv"
+    csv_dir = os.path.join("data", "comparison")
     acc_csv_file = os.path.join(csv_dir, acc_csv_filename)
     with open(acc_csv_file, mode="w") as csv_f:
         csv_writer = csv.writer(
