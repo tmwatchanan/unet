84bc82afda380c220c1aa9eeadb778efb49c2215
tmwatchanan <tmwatchanan@gmail.com>
2019-07-19 15:55:50 +0700

    Add experiment v21

--- predict_images_on_grid.py
+++ predict_images_on_grid.py
@@ -251,6 +251,29 @@ experiment_pool = {
             ),
         ],
     },
+    "eye_v5-model_v21-rgb": {
+        "input_size": (256, 256, 9),
+        "color_model": "rgb",
+        "file": "model_v21_cv",
+        "experiments": [
+            (
+                "eye_v5-model_v21_multiclass-softmax-cce-lw_1_0-rgb-fold_1-lr_1e_2-bn",
+                4520,
+            ),
+            (
+                "eye_v5-model_v21_multiclass-softmax-cce-lw_1_0-rgb-fold_2-lr_1e_2-bn",
+                4826,
+            ),
+            (
+                "eye_v5-model_v21_multiclass-softmax-cce-lw_1_0-rgb-fold_3-lr_1e_2-bn",
+                2068,
+            ),
+            (
+                "eye_v5-model_v21_multiclass-softmax-cce-lw_1_0-rgb-fold_4-lr_1e_2-bn",
+                4107,
+            ),
+        ],
+    },
 }
 
 create_model = None
@@ -278,128 +301,128 @@ def import_model_functions(filename):
 
 
 def predict():
-    for NAME in experiment_pool:
-    # NAME = "eye_v5-model_v12-rgb"
-        experiment = experiment_pool[NAME]
-        import_model_functions(experiment["file"])
-        experiment_epoch_pairs = experiment["experiments"]
-        input_size = experiment["input_size"]
-        color_model = experiment["color_model"]
-        test_dir_names = ["validation", "test"]
-        TARGET_SIZE = (256, 256)
-        NUM_CLASSES = 3
-        BATCH_NORMALIZATION = True
-        PREDICT_VERBOSE = 1  # 0 = silent, 1
+    # for NAME in experiment_pool:
+    NAME = "eye_v5-model_v15-hsv"
+    experiment = experiment_pool[NAME]
+    import_model_functions(experiment["file"])
+    experiment_epoch_pairs = experiment["experiments"]
+    input_size = experiment["input_size"]
+    color_model = experiment["color_model"]
+    test_dir_names = ["validation", "test"]
+    TARGET_SIZE = (256, 256)
+    NUM_CLASSES = 3
+    BATCH_NORMALIZATION = True
+    PREDICT_VERBOSE = 1  # 0 = silent, 1
 
-        for test_dir_name in test_dir_names:
-            fig = plt.figure(figsize=(28, 14))
-            fig.tight_layout()
-            figure_title = experiment_epoch_pairs[0][0].replace("fold_1", "4_folds")
-            figure_title += "\n" + test_dir_name
-            fig.suptitle(figure_title)
-            num_folds = len(experiment_epoch_pairs)
-            outer = gridspec.GridSpec(num_folds, 1, wspace=0.2, hspace=0.2)
+    for test_dir_name in test_dir_names:
+        fig = plt.figure(figsize=(28, 14))
+        fig.tight_layout()
+        figure_title = experiment_epoch_pairs[0][0].replace("fold_1", "4_folds")
+        figure_title += "\n" + test_dir_name
+        fig.suptitle(figure_title)
+        num_folds = len(experiment_epoch_pairs)
+        outer = gridspec.GridSpec(num_folds, 1, wspace=0.2, hspace=0.2)
 
-            for fold, (experiment_name, epoch) in enumerate(experiment_epoch_pairs):
-                inner = gridspec.GridSpecFromSubplotSpec(
-                    2, 1, subplot_spec=outer[fold], wspace=0, hspace=0.3
-                )
-
-                weight = f"{epoch:08d}"
-                cprint(f"The weight at epoch#", color="green", end="")
-                cprint(f"{weight}", color="green", attrs=["bold"], end="")
-                cprint(f" will be used to predict the images in ", color="green", end="")
-                cprint(f"{test_dir_name}", color="green", attrs=["bold"], end="")
-                cprint(f" directory", color="green")
-
-                dataset_path = os.path.join("data", experiment_name)
-                weights_dir = os.path.join(dataset_path, "weights")
-                test_set_dir = os.path.join(dataset_path, test_dir_name)
-                predicted_set_dirname = f"{test_dir_name}-predicted"
-                predicted_set_dir = os.path.join(dataset_path, predicted_set_dirname)
-
-                if not os.path.exists(predicted_set_dir):
-                    os.makedirs(predicted_set_dir)
-
-                trained_weights_filename = f"{weight}.hdf5"
-                trained_weights_file = os.path.join(weights_dir, trained_weights_filename)
-
-                # load pretrained model
-                model = create_model(
-                    pretrained_weights=trained_weights_file,
-                    input_size=input_size,
-                    num_classes=NUM_CLASSES,
-                    batch_normalization=BATCH_NORMALIZATION,
-                    is_summary=False,
-                )
-
-                # test the model
-                test_data_dict = dict(
-                    test_path=test_set_dir, target_size=TARGET_SIZE, image_color=color_model
-                )
-                test_flow, test_files = get_test_data(**test_data_dict)
-                test_gen = test_generator(test_flow, color_model)
-
-                predict_steps = len(test_files)
-                results = model.predict_generator(
-                    test_gen, steps=predict_steps, verbose=PREDICT_VERBOSE
-                )
-
-                segment_results = results[0]
-                # iris_results = results[1]
-
-                """
-                Groundtruth
-                """
-                groundtruths = []
-                for test_file in test_files:
-                    _, file = os.path.split(test_file)
-                    label_path = os.path.join(test_set_dir, "labels", file)
-                    groundtruth = skimage.io.imread(label_path)
-                    groundtruths.append(groundtruth)
-
-                row = gridspec.GridSpecFromSubplotSpec(
-                    1, len(test_files), subplot_spec=inner[0], wspace=0.2, hspace=0
-                )
-                plot_groundtruths(
-                    fig,
-                    row,
-                    groundtruths,
-                    filenames=test_files,
-                    weights_name=weight,
-                    target_size=TARGET_SIZE,
-                    num_class=NUM_CLASSES,
-                )
-
-                """
-                Predicted Outputs
-                """
-                fig.add_subplot()
-                row = gridspec.GridSpecFromSubplotSpec(
-                    1, len(test_files), subplot_spec=inner[1], wspace=0.2, hspace=0
-                )
-                plot_predicted_outputs(
-                    fig,
-                    row,
-                    segment_results,
-                    groundtruths,
-                    weights_name=weight,
-                    target_size=TARGET_SIZE,
-                    num_class=NUM_CLASSES,
-                )
-
-            plt.draw()
-            plt.pause(0.001)
-            # input("Press [enter] to continue.")
-            plt.show()
-
-            prediction_dir = os.path.join("data", "prediction")
-            if not os.path.exists(prediction_dir):
-                os.makedirs(prediction_dir)
-            figure_file = os.path.join(
-                prediction_dir, f"{figure_title}-{test_dir_name}.png"
+        for fold, (experiment_name, epoch) in enumerate(experiment_epoch_pairs):
+            inner = gridspec.GridSpecFromSubplotSpec(
+                2, 1, subplot_spec=outer[fold], wspace=0, hspace=0.3
             )
-            plt.savefig(figure_file, bbox_inches="tight")
+
+            weight = f"{epoch:08d}"
+            cprint(f"The weight at epoch#", color="green", end="")
+            cprint(f"{weight}", color="green", attrs=["bold"], end="")
+            cprint(f" will be used to predict the images in ", color="green", end="")
+            cprint(f"{test_dir_name}", color="green", attrs=["bold"], end="")
+            cprint(f" directory", color="green")
+
+            dataset_path = os.path.join("data", experiment_name)
+            weights_dir = os.path.join(dataset_path, "weights")
+            test_set_dir = os.path.join(dataset_path, test_dir_name)
+            predicted_set_dirname = f"{test_dir_name}-predicted"
+            predicted_set_dir = os.path.join(dataset_path, predicted_set_dirname)
+
+            if not os.path.exists(predicted_set_dir):
+                os.makedirs(predicted_set_dir)
+
+            trained_weights_filename = f"{weight}.hdf5"
+            trained_weights_file = os.path.join(weights_dir, trained_weights_filename)
+
+            # load pretrained model
+            model = create_model(
+                pretrained_weights=trained_weights_file,
+                input_size=input_size,
+                num_classes=NUM_CLASSES,
+                batch_normalization=BATCH_NORMALIZATION,
+                is_summary=False,
+            )
+
+            # test the model
+            test_data_dict = dict(
+                test_path=test_set_dir, target_size=TARGET_SIZE, image_color=color_model
+            )
+            test_flow, test_files = get_test_data(**test_data_dict)
+            test_gen = test_generator(test_flow, color_model)
+
+            predict_steps = len(test_files)
+            results = model.predict_generator(
+                test_gen, steps=predict_steps, verbose=PREDICT_VERBOSE
+            )
+
+            segment_results = results[0]
+            # iris_results = results[1]
+
+            """
+            Groundtruth
+            """
+            groundtruths = []
+            for test_file in test_files:
+                _, file = os.path.split(test_file)
+                label_path = os.path.join(test_set_dir, "labels", file)
+                groundtruth = skimage.io.imread(label_path)
+                groundtruths.append(groundtruth)
+
+            row = gridspec.GridSpecFromSubplotSpec(
+                1, len(test_files), subplot_spec=inner[0], wspace=0.2, hspace=0
+            )
+            plot_groundtruths(
+                fig,
+                row,
+                groundtruths,
+                filenames=test_files,
+                weights_name=weight,
+                target_size=TARGET_SIZE,
+                num_class=NUM_CLASSES,
+            )
+
+            """
+            Predicted Outputs
+            """
+            fig.add_subplot()
+            row = gridspec.GridSpecFromSubplotSpec(
+                1, len(test_files), subplot_spec=inner[1], wspace=0.2, hspace=0
+            )
+            plot_predicted_outputs(
+                fig,
+                row,
+                segment_results,
+                groundtruths,
+                weights_name=weight,
+                target_size=TARGET_SIZE,
+                num_class=NUM_CLASSES,
+            )
+
+        plt.draw()
+        plt.pause(0.001)
+        # input("Press [enter] to continue.")
+        plt.show()
+
+        prediction_dir = os.path.join("data", "prediction")
+        if not os.path.exists(prediction_dir):
+            os.makedirs(prediction_dir)
+        figure_file = os.path.join(
+            prediction_dir, f"{figure_title}-{test_dir_name}.png"
+        )
+        plt.savefig(figure_file, bbox_inches="tight")
 
 
 def plot_groundtruths(
